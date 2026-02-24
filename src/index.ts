6d30d105b66f6f252f9337506a08321cf7c44a4c
Requiem <onej4sco@gmail.com>
2026-02-24 18:49:41 +0100

    Add -w flag

--- src/index.ts
+++ src/index.ts
@@ -16,15 +16,22 @@ const countLines = (str: string): number => {
   return str.endsWith("\n") ? strLength - 1 : strLength;
 }
 
+const countWords = (str: string): number => {
+  const matches = str.match(/\S+/g);
+  return matches ? matches.length : 0;
+}
+
 try {
   const fileBuffer = fs.readFileSync(fileName)
   const fileString = fileBuffer.toString("utf8");
 
   const bytesCount = fileBuffer.byteLength;
   const linesCount = countLines(fileString);
+  const wordsCount = countWords(fileString);
 
   if (flags.includes("-c")) console.log(`${bytesCount} ${fileName}`);
   if (flags.includes("-l")) console.log(`${linesCount} ${fileName}`);
+  if (flags.includes("-w")) console.log(`${wordsCount} ${fileName}`);
 
 } catch (err) {
   console.error(`Error reading file ${err}`);
