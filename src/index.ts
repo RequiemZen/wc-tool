d8b1b1d323e2d9ce1190116144f24298a6d5bc77
Requiem <onej4sco@gmail.com>
2026-02-24 19:46:28 +0100

    Convert to async and add pipeline support

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
 import fs from "fs";
 
-const args = process.argv.slice(2);
-const matchFlags = ["-c", "-l", "-w", "-m"];
-
-const flags = args.filter((arg) => matchFlags.includes(arg));
-const fileName = args.find((arg) => !arg.startsWith("-"));
-
-if (!fileName) {
-  console.error("Error: No file specified");
-  process.exit(1);
-}
-
 const countLines = (str: string): number => {
   const strLength = str.split("\n").length;
   return str.endsWith("\n") ? strLength - 1 : strLength;
@@ -21,28 +10,58 @@ const countWords = (str: string): number => {
   return matches ? matches.length : 0;
 }
 
-const countCharacters = (str: string): number => [...str].length
+const countCharacters = (str: string): number => [...str].length;
 
-let fileBuffer: Buffer;
-let fileString: string;
+async function readStdin(): Promise<string> {
+  process.stdin.setEncoding("utf8");
 
-try {
-  fileBuffer = fs.readFileSync(fileName)
-  fileString = fileBuffer.toString("utf8");
-} catch (err) {
-  console.error(`Error reading file ${err}`);
-  process.exit(1);
+  let data = "";
+  for await (const chunk of process.stdin) {
+    data += chunk
+  };
+
+  return data;
+} 
+
+async function main() {
+  const args = process.argv.slice(2);
+  const matchFlags = ["-c", "-l", "-w", "-m"];
+  const flags = args.filter((arg) => matchFlags.includes(arg));
+  const fileName = args.find((arg) => !arg.startsWith("-"));
+
+  const useStdin = !fileName || fileName === "-";
+  
+  let fileContent: string;
+  let sourceName: string;
+
+  if (useStdin) {
+    fileContent = await readStdin();
+    sourceName = "";
+  } else {
+    let fileBuffer: Buffer;
+
+    try {
+      fileBuffer = fs.readFileSync(fileName);
+      fileContent = fileBuffer.toString("utf8");
+      sourceName = fileName;
+    } catch (err) {
+      console.error(`Error reading file ${err}`);
+      process.exit(1);
+    }
+  }
+
+  const bytesCount = Buffer.byteLength(fileContent);
+  const linesCount = countLines(fileContent);
+  const wordsCount = countWords(fileContent);
+  const characterCount = countCharacters(fileContent);
+
+  if (flags.includes("-c")) console.log(`${bytesCount} ${sourceName}`);
+  if (flags.includes("-l")) console.log(`${linesCount} ${sourceName}`);
+  if (flags.includes("-w")) console.log(`${wordsCount} ${sourceName}`);
+  if (flags.includes("-m")) console.log(`${characterCount} ${sourceName}`);
+
+  if (flags.length === 0) 
+    console.log(`${linesCount} ${wordsCount} ${bytesCount} ${sourceName}`);
 }
 
-const bytesCount = fileBuffer.byteLength;
-const linesCount = countLines(fileString);
-const wordsCount = countWords(fileString);
-const characterCount = countCharacters(fileString);
-
-if (flags.includes("-c")) console.log(`${bytesCount} ${fileName}`);
-if (flags.includes("-l")) console.log(`${linesCount} ${fileName}`);
-if (flags.includes("-w")) console.log(`${wordsCount} ${fileName}`);
-if (flags.includes("-m")) console.log(`${characterCount} ${fileName}`);
-
-if (flags.length === 0) 
-  console.log(`${linesCount} ${wordsCount} ${bytesCount} ${fileName}`)
+main();
